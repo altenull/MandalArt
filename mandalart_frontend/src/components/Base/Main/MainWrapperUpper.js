370bba92e6449b4c6875e4645f1a7c97774965c1
altenull <altenull@gmail.com>
2017-12-17 00:23:57 +0900

    added response to size

--- mandalart_frontend/src/components/Base/Main/MainWrapperUpper.js
+++ mandalart_frontend/src/components/Base/Main/MainWrapperUpper.js
@@ -23,27 +23,44 @@ const MainContents = styled.div`
     align-items: center;
     
     ${media.wide`
-        width: 100%;
+        width: 792px;
     `}
 
     ${media.tablet`
-        width: 100%;
+        width: 80%;
+    `}
+
+    ${media.phone`
+        justify-content: center;
+        flex-direction: column;
     `}
 `;
 
 const ContentL = styled.div`
     display: flex;
     flex-direction: column;
+
+    ${media.phone`
+        margin-bottom: 2rem;
+    `}
 `;
 
 const Text1 = styled.h1`
     color: ${oc.gray[1]};
     font-size: 2.6rem;
+
+    ${media.tablet`
+        font-size: 2rem;
+    `}
 `;
 
 const Text2 = styled.p`
     color: ${oc.gray[2]};
     font-size: 1.6rem;
+
+    ${media.tablet`
+      font-size: 1rem;
+    `}
 `;
 
 const ContentR = styled.div`
@@ -51,6 +68,15 @@ const ContentR = styled.div`
     height: 230px;
     display: flex;
     flex-wrap: wrap;
+
+    ${media.tablet`
+        width: 140px;
+        height: 140px;
+    `}
+
+    ${media.phone`
+        margin-top: 2rem;
+    `}
 `;
 
 const Cell = styled.div`
@@ -61,6 +87,11 @@ const Cell = styled.div`
     background: ${oc.red[5]};
     ${shadow(1)}
     margin: 3px;
+
+    ${media.tablet`
+        min-width: 40px;
+        min-height: 40px;
+    `}
 `;
 
 const CenterCell = styled.div`
@@ -71,6 +102,11 @@ const CenterCell = styled.div`
     border: 1px solid ${oc.gray[3]};
     background: linear-gradient(135deg, ${oc.orange[2]}, ${oc.red[4]});
     margin: 3px;
+
+    ${media.tablet`
+        min-width: 40px;
+        min-height: 40px;
+    `}
 `;
 
 const MainWrapperUpper = () => {

c38899cc8daa3f9432fe0edb753e7f4a233eee51
altenull <altenull@gmail.com>
2017-12-25 16:49:21 +0900

    Structured MandalArt Containers

---
+++ mandalart_frontend/src/containers/MandalArt/Write.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Write extends Component {
+    render() {
+        return (
+            <div>
+                Write<br/>
+                Write<br/>
+                Write<br/>
+                Write<br/>
+                Write<br/>
+                Write<br/>
+                Write<br/>
+                Write<br/>
+                
+            </div>
+        );
+    }
+}
+
+export default Write;

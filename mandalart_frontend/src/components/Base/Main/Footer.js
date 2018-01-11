45c50d23cfa4240aa69440df52be53f17377c2a7
altenull <altenull@gmail.com>
2018-01-11 18:54:53 +0900

    Removed shadow

--- mandalart_frontend/src/components/Base/Main/Footer.js
+++ mandalart_frontend/src/components/Base/Main/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
-import { shadow, media } from 'lib/styleUtils';
+import { media } from 'lib/styleUtils';
 
 const Positioner = styled.div`
     width: 100%;

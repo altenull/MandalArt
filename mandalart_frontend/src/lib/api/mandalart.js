3a53347ff119e27eec0066091cec9eb09dec7b1d
altenull <altenull@gmail.com>
2018-01-15 12:41:42 +0900

    Added POST Request : MandalArt Star

--- mandalart_frontend/src/lib/api/mandalart.js
+++ mandalart_frontend/src/lib/api/mandalart.js
@@ -4,3 +4,4 @@ export const mandalartGet = () => axios.get('/api/v1.0/mandalart');
 export const mandalartGetOlder = (id) => axios.get('/api/v1.0/mandalart/older/' + id);
 export const mandalartWrite = ({writer, goal, plans}) => axios.post('/api/v1.0/mandalart/write', {writer, goal, plans});
 export const mandalartDelete = (id) => axios.delete('/api/v1.0/mandalart/' + id);
+export const mandalartStar = ({id, provider}) => axios.post('/api/v1.0/mandalart/star', {id, provider});

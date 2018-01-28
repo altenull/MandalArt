b51b03c6cf204f8bfd70349cfcb78b588fdeedd3
altenull <altenull@gmail.com>
2018-01-29 02:21:26 +0900

    Solved Cross-domain problem

--- mandalart_backend/src/index.js
+++ mandalart_backend/src/index.js
@@ -17,6 +17,27 @@ const jwtMiddleware = require('lib/middlewares/jwt');
 
 db.connect();
 
+app.use((ctx, next) => {
+    const allowedHosts = [
+        'mandalart.kr',
+        'mandalart-web.s3-website.ap-northeast-2.amazonaws.com'
+    ];
+    const origin = ctx.header['origin'];
+    allowedHosts.every(el => {
+        if(!origin) return false;
+        if(origin.indexOf(el) !== -1) {
+        ctx.response.set('Access-Control-Allow-Origin', origin);
+        return false;
+        }
+        return true;
+    });
+    ctx.set('Access-Control-Allow-Credentials', true);
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-timebase, Link');
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
+    ctx.set('Access-Control-Expose-Headers', 'Link');
+    return next();
+});
+
 app.use(jwtMiddleware);
 app.use(bodyParser());
 
